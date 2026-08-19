3e3ec6209d2d5780a473f411ca0943c1aadd6880
Brad Traversy <traversymedia@gmail.com>
2026-08-19 13:06:27 -0400

    chore: bump multiprompter cask to 1.1, point at public releases repo

--- Casks/multiprompter.rb
+++ Casks/multiprompter.rb
@@ -1,13 +1,13 @@
 cask "multiprompter" do
-  version "1.0"
-  sha256 "4c339f4c82b46fdd7c458497260de1ce19e45ee4e692a774e658deb6bc2bfe29"
+  version "1.1"
+  sha256 "c2f85f8eddde37678de797422deb836f850a16b4555a8dd0597c6e9133d590b7"
 
-  url "https://github.com/bradtraversy/multiprompter/releases/download/v#{version}/Multiprompter-#{version}.zip"
+  url "https://github.com/bradtraversy/multiprompter-releases/releases/download/v#{version}/Multiprompter.dmg"
   name "Multiprompter"
   desc "Multi-window teleprompter with synced scrolling"
-  homepage "https://github.com/bradtraversy/multiprompter"
+  homepage "https://multiprompter.app/"
 
-  depends_on macos: ">= :sequoia"
+  depends_on macos: :sequoia
 
   app "Multiprompter.app"
 
